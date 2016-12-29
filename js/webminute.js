57ff5c87205b31aa46ceafa7bfc431de8516b1f0
Saahir <saahirhfoux@gmail.com>
2016-12-29 00:48:31 -0500

    another
    slimscroll tweak

--- js/webminute.js
+++ js/webminute.js
@@ -18,6 +18,6 @@ jQuery(function ($) {
     /*  #slimScroll
      /* ----------------------------------------------------------- */
     $('.project-contents').slimScroll({
-        height: '800px'
+        height: '500px'
     });
 });
