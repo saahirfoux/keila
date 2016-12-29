5a61034c00e2e262b92ea93eaeb7b2801acd5fd8
Saahir <saahirhfoux@gmail.com>
2016-12-28 23:45:41 -0500

    For Keila

---
+++ js/webminute.js
@@ -0,0 +1,23 @@
+jQuery(function ($) {
+    "use strict";
+    $('#fullpage').fullpage({
+        css3: true,
+        loopHorizontal: false,
+        controlArrows: false,
+        scrollingSpeed: 800
+    });
+    /* ----------------------------------------------------------- */
+    /*  #imgLiquid
+     /* ----------------------------------------------------------- */
+    $('.imgLiquid').imgLiquid({
+        fill: false,
+        horizontalAlign: "center",
+        verticalAlign: "center"
+    });
+    /* ----------------------------------------------------------- */
+    /*  #slimScroll
+     /* ----------------------------------------------------------- */
+    $('.project-contents').slimScroll({
+        height: '974px'
+    });
+});
